99ffee87337520e1bd30bebd96cdafe66daa3174
dlwns97 <ihjune97@naver.com>
2020-10-06 17:39:08 +0900

    push 2 files

---
+++ dlwns.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout<<"Hi clone"<<endl;
+	return 0;
+}
---
+++ dlwns2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout<<"Bye clone"<<endl;
+	return 0;
+}
